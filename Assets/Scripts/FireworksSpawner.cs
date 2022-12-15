5f8cd6a8160674db5a21c64485b8aaefd6fb41b6
GuterKase <2916675@gmail.com>
2022-12-15 20:17:51 +0300

    Added backgound and fireworks spawn system

--- Assets/Scripts/FireworksSpawner.cs
+++ Assets/Scripts/FireworksSpawner.cs
@@ -1,18 +1,49 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FireworksSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject[] _prefabs;
+
+    [SerializeField]
+    private Vector3 _position;
+
+    private float _yMax = 150;
+    private float _xMax = 225;
+
+    private bool _delayGoing;
+    private WaitForSeconds _timeout = new(0.5f);
+
+    private void Update()
     {
-        
+        if (Input.touchCount <= 0 || _delayGoing) return;
+
+        Touch touch = Input.GetTouch(0);
+
+        _position.x = GetPosition(Screen.width, touch.position.x, _xMax);
+        _position.y = GetPosition(Screen.height, touch.position.y, _yMax);
+
+        int random = Random.Range(0, _prefabs.Length);
+        Instantiate(_prefabs[random], _position, Quaternion.identity);
+        StartCoroutine(Delay());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Delay()
     {
-        
+        _delayGoing = true;
+
+        yield return _timeout;
+
+        _delayGoing = false;
+    }
+
+    private float GetPosition(float screen, float touch, float max)
+    {
+        float percent = touch * 100 / screen;
+        float position = percent * max / 100;
+
+        return position - max / 2;
     }
 }
+    
